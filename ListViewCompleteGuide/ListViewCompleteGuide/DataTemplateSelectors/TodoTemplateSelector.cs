9c860cf970155f515a4ca561ae0186c398c293cf
Damien <damientohin@gmail.com>
2018-04-11 08:49:01 +0100

    I added cells and Item Templates and selectors

---
+++ ListViewCompleteGuide/ListViewCompleteGuide/DataTemplateSelectors/TodoTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using ListViewCompleteGuide.DataTemplates;
+using ListViewCompleteGuide.Models;
+
+namespace ListViewCompleteGuide.DataTemplateSelectors
+{
+    public class TodoTemplateSelector : DataTemplateSelector
+    {
+        DataTemplate PrimaryItemTemplate;
+        DataTemplate SecondaryItemTemplate;
+
+        public TodoTemplateSelector()
+        {
+            PrimaryItemTemplate = new DataTemplate(typeof(CurrentTodoTemplate));
+            SecondaryItemTemplate = new DataTemplate(typeof(CompletedTodoTemplate));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var todo = item as Todo;
+
+            return todo.IsDone ? SecondaryItemTemplate : PrimaryItemTemplate;
+        }
+    }
+}
